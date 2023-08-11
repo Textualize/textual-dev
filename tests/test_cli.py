eeff90c378f4c165bb6de10c3160bcb52db0d9f0
Pavel Kral <pavel@pavelkral.eu>
2023-08-11 17:40:08 +0200

    Add test for keys command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,3 +8,9 @@ def test_cli_version():
     runner = CliRunner()
     result = runner.invoke(run, ["--version"])
     assert version("textual") in result.output
+
+
+def test_cli_keys():
+    runner = CliRunner()
+    result = runner.invoke(run, ["keys"])
+    assert result.exit_code == 0
